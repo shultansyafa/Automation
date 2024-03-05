710b5e5bdda398ca63066776ab316e695b2be59a
shultan syafa <shultanmaulana21@gmail.com>
2024-03-05 13:11:10 +0700

    first commit

---
+++ src/test/java/ApiTest/testingApi.java
@@ -0,0 +1,18 @@
+package ApiTest;
+
+
+import org.testng.annotations.Test;
+
+public class testingApi {
+
+    @Test
+    public void getUser(){
+
+    }
+
+
+    @Test
+    public void postCreate(){
+
+    }
+}
